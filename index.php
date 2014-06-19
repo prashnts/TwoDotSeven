d84122b87fb0d5a7978e24f60a291040939b2f1c
Prashant Sinha <prashnts@live.com>
2014-06-19 15:34:50 +0530

    Base file.

---
+++ index.php
@@ -0,0 +1,14 @@
+<?php
+#  _____                      _____ 
+# /__   \__      _____       |___  |
+#   / /\/\ \ /\ / / _ \         / / 
+#  / /    \ V  V / (_) |  _    / /  
+#  \/      \_/\_/ \___/  (_)  /_/   
+                                  
+/**
+ * This file Invokes the Main Job. 
+ * @author Prashant Sinha <prashantsinha@outlook.com>
+ * @since 0.0
+ */
+
+?>
