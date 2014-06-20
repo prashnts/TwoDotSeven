9ef3674160b1f6ee5b515aeb14df4b5c696a4515
Prashant Sinha <prashnts@live.com>
2014-06-20 15:10:15 +0530

    Database Class Done.

--- TwoDotSeven/database.php
+++ TwoDotSeven/database.php
@@ -1,19 +1,66 @@
 <?php
 namespace TwoDot7\Database;
+#  _____                      _____ 
+# /__   \__      _____       |___  |
+#   / /\/\ \ /\ / / _ \         / / 
+#  / /    \ V  V / (_) |  _    / /  
+#  \/      \_/\_/ \___/  (_)  /_/   
 
 /**
- * 
+ * Builds a Database Handler object. Requires Configuration NameSpace to Work.
+ * Handler Takes care of SQL Best Practices and Prevetion of Injection attacks by itself.
+ * @author	Prashant Sinha <firstname,lastname>@outlook.com
+ * @since	v0.0 20072014
+ * @version	0.0
  */
 class Handler {
+	/**
+	 * Contains the PDO Object.
+	 * @var PDO \TwoDot7\Database\Handler\DbHandle
+	 */
+	private $DbHandle;
+
+	/**
+	 * Contains Count of Handle Objects Created.
+	 * @var integer
+	 */
+	private static $Count = 0;
+
+	/**
+	 * Default Constructor. Constructs the Handle Object.
+	 * @return	boolean
+	 */
 	function __construct() {
 		$ConnectionString = "mysql:host=".\TwoDot7\Config\SQL_HOST.";dbname=".\TwoDot7\Config\SQL_DBNAME;
 		$SqlUsername = \TwoDot7\Config\SQL_USERNAME;
 		$SqlPassword = \TwoDot7\Config\SQL_PASSWORD;
 		try {
-			$this->DbHandle = new PDO($ConnectionString, $SqlUsername, $SqlPassword);
+			$this->DbHandle = new \PDO($ConnectionString, $SqlUsername, $SqlPassword);
+			Handler::$Count++;
 			return TRUE;
 		}
+		catch (\PDOException $E) {
+			return FALSE;
+		}
+	}
+
+	/**
+	 * Executes a Query.
+	 * @param string $Query Formed Query String.
+	 * @param array $Arr Optional. Contains $Query parameters.
+	 * @return \PDO Binding.
+	 */
+	public function Query ($Query, $Arr = FALSE) {
+		try {
+			$this->DbHandle->beginTransaction();
+			$Binding = $this->DbHandle->prepare($Query);
+			$Binding->execute($Arr);
+			$this->DbHandle->Commit();
+			return ($Binding);
+		}
 		catch (PDOException $E) {
+			afPutLogEntry ($E->getMessage(), DEBUG);
+			$this->DbHandle->rollback();
 			return FALSE;
 		}
 	}
