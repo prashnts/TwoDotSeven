96dba39439cb6cef601a46e89f8a3bca125bedd3
Prashant Sinha <prashnts@live.com>
2014-06-20 15:19:01 +0530

    Created Base Installer File.

---
+++ TwoDotSeven/install.php
@@ -0,0 +1,25 @@
+<?php
+namespace TwoDot7\Install;
+#  _____                      _____ 
+# /__   \__      _____       |___  |
+#   / /\/\ \ /\ / / _ \         / / 
+#  / /    \ V  V / (_) |  _    / /  
+#  \/      \_/\_/ \___/  (_)  /_/   
+
+/**
+ * Sets Up the Environment for Setting up the Website.
+ * @author	Prashant Sinha <firstname,lastname>@outlook.com
+ * @since	v0.0 20072014
+ * @version	0.0
+ */
+class Installer {
+
+	/**
+	 * Sets up the Tables in SQL.
+	 * @param boolean $CLI Toggles echo Mode;
+	 */
+	function private BuildTables() {
+
+	}
+}
+?>
