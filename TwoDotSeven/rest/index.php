026f4c9e11c68a545098760a3055aa9346d82b27
Prashant Sinha <prashnts@live.com>
2014-06-26 02:04:55 +0530

    Oh wait. Fine, now.

--- TwoDotSeven/rest/index.php
+++ TwoDotSeven/rest/index.php
@@ -1,4 +1,5 @@
 <?php
+namespace TwoDot7\REST;
 #  _____                      _____ 
 # /__   \__      _____       |___  |
 #   / /\/\ \ /\ / / _ \         / / 
@@ -6,9 +7,56 @@
 #  \/      \_/\_/ \___/  (_)  /_/   
                                   
 /**
- * This Invokes the AJAX api calls. 
+ * This Invokes and processes the AJAX api calls. 
  * @author Prashant Sinha <prashantsinha@outlook.com>
- * @author Tarun Khajuria <tarunkhajuria42@gmail.com>
  * @since 0.0
  */
+
+require_once "apiconfig.php";
+require_once "../config.php";
+require_once "../inc/cron.php";
+require_once "../inc/database.php";
+require_once "../inc/exceptions.php";
+require_once "../inc/install.php";
+require_once "../inc/templatehandler.php";
+require_once "../inc/user.php";
+require_once "../inc/utility.php";
+require_once "../inc/validator.php";
+
+# Parse incoming URI and then process it.
+$URI = preg_replace("/[\/]+/", "/", $_SERVER['REQUEST_URI']);
+$URIparse = explode('/', $URI);
+
+switch(strtolower(isset($URIparse[2]) ? $URIparse[2] : False)) {
+	case 'getqr':
+		# Parse URI template: one.ducic.ac.in/dev/getqr/[Encrypted QR Data, padded under serialized array.]
+		$_GET = array(
+			'Content' => isset($URIparse[3]) ? $URIparse[3] : False,
+			'Auth' => isset($URIparse[4]) ? $URIparse[4] : False);
+		__initQR();
+		break;
+
+	case 'user':
+		# Parse URI template: one.ducic.ac.in/dev/user/[username]/[getprofile/[public, private, mini], postprofile]
+		$_GET = array(
+			'UserName' => isset($URIparse[3]) ? $URIparse[3] : False);
+			__initUser();
+			break;
+
+	case 'ajax':
+		# Parse URI template: one.ducic.ac.in/dev/ajax/[function]
+		$_GET = array(	'Function'		=>	isset($URIparse[4]) ? $URIparse[4] : false);
+		__initAjax();
+		break;
+
+	default:				
+		http_response_code(400);
+		header('API-Generated-By: The awesome TwoDotSeven API Engine.');
+		header('API-Message: Just saying Hi. You have made a request that is incomplete. Please re-check it.');
+		header('API-Author: Prashant Sinha');
+		echo '<pre>';
+		echo 'You made an incomplete request to TwoDotSeven api. Please consider reading the API documentation.';
+		echo '</pre>';
+}
+
 ?>
