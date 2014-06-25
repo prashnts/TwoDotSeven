d06b2fc6378c649186f3e504af89fd0c8ac046aa
Prashant Sinha <prashnts@live.com>
2014-06-26 01:35:30 +0530

    Started AJAX API Call handeler. Fixed Config files.

--- TwoDotSeven/config_sample.php
+++ TwoDotSeven/config_sample.php
@@ -7,11 +7,11 @@ namespace TwoDot7\Config;
 #  \/      \_/\_/ \___/  (_)  /_/   
 
 # Put the Domain Name, and the Root directory name.
-define("BASEURI",		"http://localhost:70");
-define("ROOTDIR",		"--Fill Your Info Here --");
+define("BASEURI", "http://localhost:70");
+define("ROOTDIR", "--Fill Your Info Here --");
 
 # Default Language files.
-define("LANG_EN",		"langEN.php");
+define("LANG_EN", "langEN.php");
 
 # SQL Configuration.
 const SQL_HOST = "--Fill Your Info Here --";
@@ -20,17 +20,17 @@ const SQL_PASSWORD = "--Fill Your Info Here --";
 const SQL_DBNAME = "--Fill Your Info Here --";
 
 # Encryption Keys. Generate them on <todo>
-const SALTKEY = "--Fill Your Info Here --";
-const ENTROPY = 3;
-
+# WARNING: CHANGING THESE VALUES RENDERS ALL THE ENCRYPTED DATA USELESS.
 const SHA_PRIVATE = "--Fill Your Info Here --";
 const SHA_INITIAL_VECTOR = "--Fill Your Info Here --";
 
 # Optional Offsets.
-define("TIME_OFFSET", 9800);			# Default Offset to GMT +5:30, Indian time zone. Set 0 for GMT.
+# Default Offset to GMT +5:30, Indian time zone. Set 0 for GMT.
+define("TIME_OFFSET", 9800);
+# Maximum number of Concurrent logins.
 define("MAXIMUM_CONCURRENT_LOGINS", 5);
 
 # Environment Settings.
-error_reporting(E_ERROR | E_WARNING | E_PARSE | E_NOTICE); 	# Comment it out in Production Environment.
+error_reporting(E_ALL); 	# Comment it out in Production Environment.
 #error_reporting(0); # Comment it out in Development Environment.
 ?>
---
+++ TwoDotSeven/rest/index.php
@@ -0,0 +1,14 @@
+<?php
+#  _____                      _____ 
+# /__   \__      _____       |___  |
+#   / /\/\ \ /\ / / _ \         / / 
+#  / /    \ V  V / (_) |  _    / /  
+#  \/      \_/\_/ \___/  (_)  /_/   
+                                  
+/**
+ * This Invokes the AJAX api calls. 
+ * @author Prashant Sinha <prashantsinha@outlook.com>
+ * @author Tarun Khajuria <tarunkhajuria42@gmail.com>
+ * @since 0.0
+ */
+?>
