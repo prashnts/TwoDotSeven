402a652460bb5ad8cea696c36b7b85009949800b
Prashant Sinha <prashnts@live.com>
2014-08-26 17:16:07 +0530

    PHP Mailer

--- TwoDotSeven/inc/mailer.php
+++ TwoDotSeven/inc/mailer.php
@@ -10,4 +10,27 @@ use \TwoDot7\Util as Util;
 
 function Send($Data) {
 	Util\Log(json_encode($Data));
+	_Import($_SERVER['DOCUMENT_ROOT']."/TwoDotSeven/inc/external/PHPMailer/PHPMailerAutoload.php");
+	date_default_timezone_set('Etc/UTC');
+	$mail = new \PHPMailer();
+	$mail->isSMTP();
+	$mail->SMTPDebug = 0;
+	$mail->Host = 'smtp.gmail.com';
+	$mail->Port = 587;
+	$mail->SMTPSecure = 'tls';
+	$mail->SMTPAuth = true;
+	$mail->Username = "prashantinq@gmail.com";
+	$mail->Password = "para-xylene";
+	$mail->setFrom('from@example.com', 'First Last');
+	$mail->addReplyTo('replyto@example.com', 'First Last');
+	$mail->addAddress('prashantsinha@outlook.com', 'John Doe');
+	$mail->Subject = 'PHPMailer GMail SMTP test';
+	$mail->msgHTML("Hello World.");
+	$mail->AltBody = 'This is a plain-text message body';
+
+	if (!$mail->send()) {
+	    //echo "Mailer Error: " . $mail->ErrorInfo;
+	} else {
+	    //echo "Message sent!";
+	}
 }
