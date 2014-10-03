3aedc4d8620050eea8bfac66dadac3ccd3f976f8
Prashant Sinha <prashnts@live.com>
2014-10-03 21:59:20 +0530

    Groups.

--- TwoDotSeven/inc/groups.php
+++ TwoDotSeven/inc/groups.php
@@ -10,60 +10,65 @@ use \TwoDot7\Database as Database;
 #  \/      \_/\_/ \___/  (_)  /_/   
 
 class Instance {
-	private static $Count = 0;
-	private $GroupID;
-	private $Name;
-	private $Meta;
-	private $Type;
-	private $Graph;
+    private static $Count = 0;
+    private $GroupID;
+    private $Name;
+    private $Meta;
+    private $Type;
+    private $Graph;
 
-	function __construct($GroupID) {
-		// Constructs the Group Instance. Caches for faster execution.
-		$Query = "SELECT * FROM _group WHERE GroupID = :GroupID";
-		
-		$Response = Database\Handler::Exec($Query)->fetch();
+    function __construct($GroupID) {
+        // Constructs the Group Instance. Caches for faster execution.
+        $Query = "SELECT * FROM _group WHERE GroupID = :GroupID";
+        
+        $Response = Database\Handler::Exec($Query)->fetch();
 
-		if (count($Response)) {
-			$this->GroupID = $Response['GroupID'];
-			$this->Name = $Response['Name'];
-			$this->Meta = $Response['Meta'];
-			$this->Type = $Response['Type'];
-			$this->Graph = $Response['Graph'];
-		}
-	}
+        if (count($Response)) {
+            $this->GroupID = $Response['GroupID'];
+            $this->Name = $Response['Name'];
+            $this->Meta = $Response['Meta'];
+            $this->Type = $Response['Type'];
+            $this->Graph = $Response['Graph'];
+        }
+    }
 
-	public function GetUser($UserName) {
-		// Returns user rights and checks if user is part of the group.
-		// Used to show the User in the Node.
-	}
+    public function GetUser($UserName) {
+        // Returns user rights and checks if user is part of the group.
+        // Used to show the User in the Node.
+    }
 
-	public function GetGraph() {
-		// Returns user graph, including the Users in the Group.
-	}
+    public function GetGraph() {
+        // Returns user graph, including the Users in the Group.
+    }
 
-	public function GetBroadcast() {
-		// Returns the Broadcasts targeted for this group.
-	}
-}
-
-class User extends Instance {
-	function 
+    public function GetBroadcast() {
+        // Returns the Broadcasts targeted for this group.
+    }
 }
 
 class Setup {
-	public static function Create($Data) {
-		if (!isset($Data['GroupID']),
-			!isset($Data['Type']),
-			!isset($Data['']))
-	}
+    /**
+     * Creates the Group, and returns the Generated GroupID.
+     * @param Mixed $Data Optional. Overrides default configuration:
+     *                    Mixed Meta, Specify the Default Meta Array.
+     *                    String Admin, The Group Admin. (By default, the logged-in user.)
+     *                    Mixed Graph, The User Graph, a 1D Matrix, generated through Util/Array.
+     */
+    public static function Create($Data = array()) {
+        if (isset($Data['GroupID'])) throw new \TwoDot7\Exception\InvalidArgument("GroupID is generated automatically.");
+        $Defaults = array(
+            'GroupID' => "_grp_".\TwoDot7\Util\Crypt::RandHash(),
+            'Meta' => array(),
+            'Admin' => \TwoDot7\User\Session::Data()['UserName'],
+            'Graph' => array()
+        );
 
-	public static function Delete() {
-		// Deletes the Group.
-	}
+        $Data = array_replace($Defaults, $Data);
 
-	public static function Update() {
-		// Updates the Group.
-	}
+        echo json_encode($Data);
+    }
+
+    public static function Delete($GroupID) {
+        // Deletes the Group.
+    }
 }
-
-?>
