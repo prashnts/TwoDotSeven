e8a0426ec332f0de1c5bc7c533154851408f1b4d
Prashant Sinha <prashnts@live.com>
2014-10-20 21:35:40 +0530

    Groups UI, snail pace.

--- TwoDotSeven/admin/group.php
+++ TwoDotSeven/admin/group.php
@@ -21,16 +21,11 @@ function init() {
     if ($Group->Success) {
         echo "yes, init group.";
     } else {
-        echo "nope.";
+        if ($_GET['GroupID']) {
+            echo "No, That group doesnt exists. Create new group?";
+        } else {
+            echo "Show group list, admin options.";
+        }
     }
     die();
-
-    \TwoDot7\Admin\Template\Dash_Broadcasts_Bits\_init(array(
-        'Page' => 'PRE_GROUP',
-        'Call' => 'Group',
-        'BroadcastEnabled' => \TwoDot7\User\Status::Correlate(11, \TwoDot7\User\Status::Get(\TwoDot7\User\Session::Data()['UserName'])),
-        'Navigation' => \TwoDot7\Meta\Navigation::Get(array(
-            'Page' => 'PRE_GROUP'   
-            ))
-        ));
 }
