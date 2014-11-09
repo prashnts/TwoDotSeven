1462defe73040893eb76ad0697f801bc4e7c7944
Prashant Sinha <prashnts@live.com>
2014-11-09 14:37:19 +0530

    Broadcast card CSS, and Notifications.

---
+++ TwoDotSeven/inc/notification.php
@@ -0,0 +1,9 @@
+<?php
+namespace TwoDot7\Notification;
+use \TwoDot7\Mailer as Mailer;
+use \TwoDot7\Util as Util;
+#   _____                      _____ 
+# /__   \__      _____       |___  |
+#   / /\/\ \ /\ / / _ \         / / 
+#  / /    \ V  V / (_) |  _    / /  
+#  \/      \_/\_/ \___/  (_)  /_/   
