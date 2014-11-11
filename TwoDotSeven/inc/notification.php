6a85627e079e50e8733281996124dd5c9715e8d4
Prashant Sinha <prashnts@live.com>
2014-11-11 19:00:54 +0530

    Notification Classes. Tested.

--- TwoDotSeven/inc/notification.php
+++ TwoDotSeven/inc/notification.php
@@ -38,6 +38,9 @@ const READ = 1;
  */
 const UNREAD = 2;
 
+/**
+ * The notification object.
+ */
 class Notification {
     /**
      * Notification Field
@@ -87,8 +90,36 @@ class Notification {
      * @var Boolean
      */
     private $Mode;
+
+    /**
+     * Contains the Error info, if it happens to be.
+     * @var boolean
+     */
     public $Error = False;
 
+    /**
+     * Creates the Notification object.
+     * @param   Mixed                     $Hook       Determines the type of Notification object.
+     *                                                There are two types of notification objects, the
+     *                                                New Notification, and the Existing notification.
+     *                                                The only difference between them is that while
+     *                                                creating New notification, you provide the Other
+     *                                                parameters, and pass this parameter a NULL, or
+     *                                                True, or False. If you want to fetch a
+     *                                                notification from the Database, simply supply
+     *                                                the Notification ID (int).
+     * @param   Const                     $OriginType The type of the originator, who has emitted the 
+     *                                                notification.
+     * @param   String                    $Origin     The UserName, or BitID, or GroupID, or the
+     *                                                SubSystem.
+     * @param   String                    $Target     The UserName of person for which the
+     *                                                notification object is created.
+     * @param   Const                     $Active     Whether the notification is read or unread.
+     * @param   \TwoDot7\Util\Dictionary  $Data       The Notification data. Should be the TwoDot7
+     *                                                Dictionary Object.
+     * @return  Boolean
+     * @throws  \TwoDot7\Exception\InvalidArgument If $Hook is not either (int, bool, null).
+     */
     function __construct($Hook, $OriginType = NULL, $Origin = NULL, $Target = NULL, $Active = NULL, $Data = NULL) {
         if (is_bool($Hook) || is_null($Hook)) {
 
@@ -106,7 +137,7 @@ class Notification {
 
         } elseif (is_numeric($Hook)) {
             $Response = \TwoDot7\Database\Handler::Exec("SELECT * FROM _activity WHERE ID=:ID;", array("ID" => $Hook));
-            if ((int)$Response->errorCode() === 0) {
+            if ((int)$Response->errorCode()[0] === 0) {
 
                 $this->Mode = False;
 
@@ -126,6 +157,12 @@ class Notification {
         } else throw new \TwoDot7\Exception\InvalidArgument("ID is not a valid argument.");
     }
 
+    /**
+     * Pushes the notification object into the Database.
+     * IMPORTANT: This method is not valid for Existing Notification objects.
+     * @return Boolean Whether "pushed" or not.
+     * @throws \TwoDot7\InvalidMethod If This object is Existing notification.
+     */
     public function Push() {
         if (!$this->Mode) throw new \TwoDot7\Exception\InvalidMethod("Push is not a valid Method for objects of this type.");
         return (int)\TwoDot7\Database\Handler::Exec(
@@ -137,9 +174,13 @@ class Notification {
                 'Active' => $this->Active,
                 'Data' => $this->Data->get(False, True),
                 'Timestamp' => $this->Timestamp
-            ))->errorInfo() === 0;
+            ))->errorInfo()[0] === 0;
     }
 
+    /**
+     * Returns the Notification object as a TwoDot7 dictionary.
+     * @return \TwoDot7\Util\Dictionary
+     */
     public function Get() {
         return new \TwoDot7\Util\Dictionary(array(
                 'ID' => $this->ID,
@@ -166,11 +207,11 @@ class Create {
      * @return Boolean
      */
     public static function User($Target, $Data) {
-        $Origin = \TwoDot7\User\Session\Data()['UserName'];
+        if (!\TwoDot7\User\Session::Exists()) return False;
 
-        if (!$Origin) return False;
+        $Origin = \TwoDot7\User\Session::Data()['UserName'];
 
-        $Response = False;
+        $Response = True;
         foreach ($Target->get() as $Target) {
             $Notification = new Notification(
                 True,
@@ -185,8 +226,16 @@ class Create {
         return $Response;
     }
 
+    /**
+     * Creates a notification ORIGINATING from the Bit.
+     * @param \TwoDot7\Util\_List       $Target TwoDot7 list of UserNames.
+     * @param String                    $BitID  The ID of Bit from where the notification is originating.
+     * @param \TwoDot7\Util\Dictionary  $Data   TwoDot7 dictionary containing fields, 
+     *                                          and the relevant data. Cannot be HTML.
+     * @return Boolean
+     */
     public static function Bit($Target, $BitID, $Data) {
-        $Response = False;
+        $Response = True;
         foreach ($Target->get() as $Target) {
             $Notification = new Notification(
                 True,
@@ -201,8 +250,16 @@ class Create {
         return $Response;
     }
 
+    /**
+     * Creates a notification ORIGINATING from the Group.
+     * @param \TwoDot7\Util\_List       $Target     TwoDot7 list of UserNames.
+     * @param String                    $GroupID    The ID of Group.
+     * @param \TwoDot7\Util\Dictionary  $Data       TwoDot7 dictionary containing fields, 
+     *                                              and the relevant data. Cannot be HTML.
+     * @return Boolean
+     */
     public static function Group($Target, $GroupID, $Data) {
-        $Response = False;
+        $Response = True;
         foreach ($Target->get() as $Target) {
             $Notification = new Notification(
                 True,
@@ -217,8 +274,16 @@ class Create {
         return $Response;
     }
 
+    /**
+     * Creates a notification ORIGINATING from the System.
+     * @param \TwoDot7\Util\_List       $Target     TwoDot7 list of UserNames.
+     * @param String                    $SystemName The Name of system subprocess.
+     * @param \TwoDot7\Util\Dictionary  $Data       TwoDot7 dictionary containing fields, 
+     *                                              and the relevant data. Cannot be HTML.
+     * @return Boolean
+     */
     public static function System($Target, $SystemName, $Data) {
-        $Response = False;
+        $Response = True;
         foreach ($Target->get() as $Target) {
             $Notification = new Notification(
                 True,
@@ -235,35 +300,52 @@ class Create {
 }
 
 class Service {
-    function GetAll() {
+    public static function GetAll() {
         //
     }
 
-    function GetNew() {
+    public static function GetNew() {
         //
     }
 
-    function Get() {
+    public static function Get() {
         //
     }
 
-    function SetRead() {
-        //
+    public static function SetRead($ID) {
+        $Response = \TwoDot7\Database\Handler::Exec(
+            "UPDATE _activity SET Active=:Active WHERE ID=:ID;",
+            array(
+                'ID' => $ID,
+                'Active' => READ
+            )
+        );
+        return (bool)((int)((int)$Response->errorCode()[0] === 0) + $Response->rowCount());
     }
 
-    function SetUnread() {
-        //
+    public static function SetUnread($ID) {
+        $Response = \TwoDot7\Database\Handler::Exec(
+            "UPDATE _activity SET Active=:Active WHERE ID=:ID;",
+            array(
+                'ID' => $ID,
+                'Active' => UNREAD
+            )
+        );
+        return (bool)((int)((int)$Response->errorCode()[0] === 0) + $Response->rowCount());
     }
 
-    function Delete() {
-        //
+    public static function Delete($ID) {
+        $Response = \TwoDot7\Database\Handler::Exec(
+            "DELETE FROM _activity WHERE ID=:ID;",
+            array('ID' => $ID));
+        return (bool)((int)((int)$Response->errorCode()[0] === 0) + $Response->rowCount());
     }
 
-    function DeleteAll() {
-        //
+    public static function DeleteAll($Critaria) {
+        // PROPOSED. Not to be implemented before the "Fuzzy" logic.
     }
 
-    function EMail() {
+    public static function EMail() {
         // PROPOSED. Not to be implemented anytime soon.
     }
 }
